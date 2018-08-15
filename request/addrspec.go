9a4698dbd70cd6c1f8af2a77545bf8e01ae55d69
Vilsol <me@vil.so>
2018-08-15 05:45:02 +0300

    Add address spec

--- request/addrspec.go
+++ request/addrspec.go
@@ -1,24 +1,88 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
 )
 
+const (
+	ipv4AddressType = byte(0x01)
+	fqdnAddressType = byte(0x03)
+	ipv6AddressType = byte(0x04)
+)
+
 type AddrSpec struct {
-	Host string
+	FQDN string
 	IP   net.IP
 	Port uint8
 }
 
 func (a *AddrSpec) String() string {
-	if a.Host != "" {
-		return fmt.Sprintf("%s (%s):%d", a.Host, a.IP, a.Port)
+	if a.FQDN != "" {
+		return fmt.Sprintf("%s:%d", a.FQDN, a.Port)
 	}
 	return fmt.Sprintf("%s:%d", a.IP, a.Port)
 }
 
-func GetAddrSpec(reader *io.Reader) (AddrSpec, error) {
+func GetAddrSpec(reader *io.Reader) (*AddrSpec, error) {
+	var address *AddrSpec
 
+	addressType := make([]byte, 1)
+
+	if _, err := (*reader).Read(addressType); err != nil {
+		return nil, err
+	}
+
+	switch addressType[0] {
+	case ipv4AddressType:
+		fallthrough
+	case ipv6AddressType:
+		length := 4
+
+		if addressType[0] == ipv6AddressType {
+			length = 16
+		}
+
+		ipAddress := make([]byte, length)
+
+		if _, err := io.ReadAtLeast(*reader, ipAddress, length); err != nil {
+			return nil, err
+		}
+
+		address = &AddrSpec{
+			IP: net.IP(ipAddress),
+		}
+	case fqdnAddressType:
+		addressLength := make([]byte, 1)
+
+		if _, err := (*reader).Read(addressLength); err != nil {
+			return nil, err
+		}
+
+		fqdn := make([]byte, addressLength[0])
+
+		if _, err := io.ReadAtLeast(*reader, fqdn, int(addressLength[0])); err != nil {
+			return nil, err
+		}
+
+		address = &AddrSpec{
+			FQDN: string(fqdn),
+		}
+	}
+
+	if address == nil {
+		return nil, errors.New("could not read address host")
+	}
+
+	port := make([]byte, 2)
+
+	if _, err := io.ReadAtLeast(*reader, port, 2); err != nil {
+		return nil, err
+	}
+
+	address.Port = uint8((int(port[0]) << 8) | int(port[1]))
+
+	return address, nil
 }
